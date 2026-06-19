28cd6ae763a99b9346090f5f32ed9655df9203d2
Panagiotis Stergioulas-Bolis <p.stergioulas@atc.gr>
2026-06-19 14:39:50 +0300

    Feat - integrate react big calendar

--- src/features/calendar/index.tsx
+++ src/features/calendar/index.tsx
@@ -1,5 +1,82 @@
+import {
+  Calendar,
+  dateFnsLocalizer,
+  type stringOrDate,
+  type View,
+} from "react-big-calendar";
+import { format, parse, startOfWeek, getDay, getTime } from "date-fns";
+import { enUS } from "date-fns/locale/en-US";
+import { Stack } from "@mui/material";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import { useCallback, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+const locales = {
+  en: enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  getTime,
+  locales,
+});
+
 const CalendarView = () => {
-  return <>calendar</>;
+  const { t, i18n } = useTranslation();
+
+  const messages = {
+    today: t("calendar.today"),
+    previous: t("calendar.previous"),
+    next: t("calendar.next"),
+    month: t("calendar.month"),
+    week: t("calendar.week"),
+    day: t("calendar.day"),
+    agenda: t("calendar.agenda"),
+    date: t("calendar.date"),
+    time: t("calendar.time"),
+    event: t("calendar.event"),
+    noEventsInRange: t("calendar.noEventsInRange"),
+  };
+
+  const culture = i18n.language === "el" ? "el" : "en-US";
+  const [view, setView] = useState<View>("month");
+  const events = [
+    {
+      title: "Test Event",
+      start: new Date("2026-06-19"),
+      end: new Date("2026-06-19"),
+    },
+  ];
+  const [date, setDate] = useState<stringOrDate>(new Date());
+
+  const onNavigate = useCallback(
+    (newDate: stringOrDate) => setDate(newDate),
+    [setDate],
+  );
+
+  const onView = useCallback((newView: View) => setView(newView), [setView]);
+
+  return (
+    <Stack sx={{ width: "100%", p: 4 }}>
+      <Calendar
+        culture={culture}
+        messages={messages}
+        view={view}
+        events={events}
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 64px)", width: "100%" }}
+        onView={onView}
+        defaultDate={date}
+        onNavigate={onNavigate}
+      />
+    </Stack>
+  );
 };
 
 export default CalendarView;
